dfcc31050f71566eb74109a49007a964c1395efd
Johnnyr81 <emailjohn81@gmail.com>
2020-02-10 01:24:06 -0600

    uninstall node-pre-gyp

--- data/migrations/20200208001614_UserTable.js
+++ data/migrations/20200208001614_UserTable.js
@@ -2,6 +2,9 @@ exports.up = function(knex) {
     return knex.schema.createTable("users", function(table) {
         table.increments();
 
+        table.string("full_name", 128)
+             .notNullable();  
+
         table.string("username", 128)
              .notNullable()
              .unique();
@@ -9,6 +12,8 @@ exports.up = function(knex) {
         table.string("password", 128).notNullable();     
         
         table.timestamps(true, true);
+        // table.datetime("created_at");
+        // table.datetime("updated_at");
              
     });
 };
