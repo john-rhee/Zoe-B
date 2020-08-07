47a6aeea845ce4c6e030328b78b7a1ffd920c2d2
Johnnyr81 <emailjohn81@gmail.com>
2020-08-07 03:04:33 -0500

    fixed

--- api/server.js
+++ api/server.js
@@ -4,6 +4,23 @@ const db = require('../data/db-config.js')
 const cors = require('cors');
 require('dotenv').config()
 
+const multer = require('multer');
+const path = require('path');
+
+//storage
+const storage = multer.diskStorage({
+    destination: './uploads',
+    filename:(req, file, cb)=>{
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({
+    storage: storage
+})
+//
+
+
 const UserRouter = require('../zoe/user-router.js');
 const TodoRouter = require('../zoe/todo-router.js');
 
@@ -22,21 +39,35 @@ server.use('/todo', TodoRouter);
 
 
 //
-var fileupload = require("express-fileupload");
-server.use(fileupload());
+server.use('/profile', express.static('/uploads'))
+
 //
-server.post("/upload", function(req, res, next){
-    const file = req.files.uImage;
-    file.mv('./uploads/'+file.name, function(err,result){
-        if(err)
-            throw err;
-        res.send({
-            seccess: true,
-            message: "File uploaded!",
-            picture: file
-        });
-    });
+// var fileupload = require("express-fileupload");
+
+// server.use(fileupload());
+
+//
+server.post("/upload", upload.single("uImage"), function(req, res){
+    console.log(req.file);
+    res.json({
+                    url: `https://zoe-backend.herokuapp.com/uploads/${req.file.filename}`,
+                    picture: req.file
+                });
 })
+    
+
+// server.post("/upload", function(req, res, next){
+//     const file = req.files.uImage;
+//     file.mv('./uploads/'+file.name+"-"+Date.now(), function(err,result){
+//         if(err)
+//             throw err;
+//         res.send({
+//             success: true,
+//             message: "File uploaded!",
+//             picture: file
+//         });
+//     });
+// })
 //
 
 module.exports = server;
