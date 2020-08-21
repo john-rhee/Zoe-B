13db7d76f98ab59573ef8ac5693555510b1e9619
Johnnyr81 <emailjohn81@gmail.com>
2020-08-21 18:56:15 -0500

    update

--- api/server.js
+++ api/server.js
@@ -18,6 +18,18 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header("Access-Control-Allow-Headers", '*');
+    if (req.method==="OPTIONS"){
+        res.header("Access-Control-Allow-Methods","*");
+        return res.status(200).json({});
+        }
+    next();
+    });
+
 
 server.get("/", (req, res) => {
     res.send({ api: "api is running..."})
