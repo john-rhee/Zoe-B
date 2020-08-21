ef3839d8f4fd27c71ef5d5ab282c9a87145474cc
Johnnyr81 <emailjohn81@gmail.com>
2020-08-20 22:41:49 -0500

    cors

--- api/server.js
+++ api/server.js
@@ -12,16 +12,15 @@ const PictureRouter = require('../zoe/picture-router.js');
 
 const server = express();
 
+const corsOptions = {
+    origin: 'https://zoe-kappa.now.sh',
+  }
+
 server.use('/profile', express.static('uploads'))
 
 server.use(helmet());
 server.use(express.json());
-server.use(cors());
-
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-  });
+server.use(cors(corsOptions));
 
 server.get("/", (req, res) => {
     res.send({ api: "api is running..."})
