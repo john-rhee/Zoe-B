87912907ea7481740964b7a4c66a8aea3013ae63
Johnnyr81 <emailjohn81@gmail.com>
2020-08-21 19:11:01 -0500

    fixing cors

--- api/server.js
+++ api/server.js
@@ -16,16 +16,8 @@ server.use('/profile', express.static('uploads'))
 
 server.use(helmet());
 server.use(express.json());
-// server.use(cors());
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-    });
-
+server.use(cors());
+server.options('*', cors())
 
 server.get("/", (req, res) => {
     res.send({ api: "api is running..."})
