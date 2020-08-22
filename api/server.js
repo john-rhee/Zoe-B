c38f3db69642699bb17f6142b9dc8f19f62bf027
Johnnyr81 <emailjohn81@gmail.com>
2020-08-21 19:01:00 -0500

    fixing cors

--- api/server.js
+++ api/server.js
@@ -21,12 +21,8 @@ server.use(express.json());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', '*');
-    res.header("Access-Control-Allow-Headers", '*');
-    if (req.method==="OPTIONS"){
-        res.header("Access-Control-Allow-Methods","*");
-        return res.status(200).json({});
-        }
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
     });
 
