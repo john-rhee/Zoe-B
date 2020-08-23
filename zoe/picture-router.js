a6fd3576530c49ddcb4b2a0d5418e795269daf41
Johnnyr81 <emailjohn81@gmail.com>
2020-08-23 03:51:33 -0500

    cors

--- zoe/picture-router.js
+++ zoe/picture-router.js
@@ -113,13 +113,13 @@ router.delete('/:id',
     Picture.removePicture(id)
     .then(deleted => {
         if (deleted) {
-        res.header("Access-Control-Allow-Origin", "*") 
+        res.header("Access-Control-Allow-Origin", "http://localhost:5000") 
         res.header(
             "Access-Control-Allow-Headers",
-            "Origin, X-Requested-With, Content-Type, Accept, Authorization" 
+            "*" 
         );
         if(req.method === 'OPTIONS'){
-            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET'); 
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE, GET'); 
             return res.status(200).json({});
         } 
         res.json({ removed: deleted });
