7d3c1aba681a9461fb54ae43daed844a1c8c915b
Johnnyr81 <emailjohn81@gmail.com>
2020-08-22 21:33:14 -0500

    cors

--- api/server.js
+++ api/server.js
@@ -14,8 +14,9 @@ const server = express();
 
 server.use(helmet());
 server.use(express.json());
-server.use(cors());
-server.options('*', cors());
+
+// server.use(cors());
+// server.options('*', cors());
 
 // server.use(function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
@@ -26,6 +27,32 @@ server.options('*', cors());
 //     next();
 //     });
 
+server.use(function(req, res, next) {
+    var oneof = false;
+    if(req.headers.origin) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        oneof = true;
+    }
+    if(req.headers['access-control-request-method']) {
+        res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+        oneof = true;
+    }
+    if(req.headers['access-control-request-headers']) {
+        res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+        oneof = true;
+    }
+    if(oneof) {
+        res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+    }
+
+    // intercept OPTIONS method
+    if (oneof && req.method == 'OPTIONS') {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+});
 
 server.get("/", (req, res) => {
     res.send({ api: "api is running..."})
