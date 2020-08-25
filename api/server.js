f37fbe87b0e26998b47375ccc051163e399e56c2
Johnnyr81 <emailjohn81@gmail.com>
2020-08-25 02:38:01 -0500

    cors

--- api/server.js
+++ api/server.js
@@ -15,8 +15,18 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
-server.use(cors());
-server.options('*', cors());
+// server.use(cors());
+// server.options('*', cors());
+
+server.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
+    res.header("Access-Control-Allow-Headers", '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    // res.header("Access-Control-Allow-Credentials", true);
+    // res.header("Access-Control-Allow-Headers", '*');
+
+    next();
+    });
 
 server.get("/", (req, res) => {
     res.send({ api: "api is running..."})
