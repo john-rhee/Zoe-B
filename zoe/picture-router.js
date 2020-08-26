62126c60e02661dbfc3a7206d963fddd1be1fb46
Johnnyr81 <emailjohn81@gmail.com>
2020-08-26 03:44:00 -0500

    before cleanup

--- zoe/picture-router.js
+++ zoe/picture-router.js
@@ -135,7 +135,7 @@ router.delete('/:id',
     console.log("deleting", file_name)
     //deleting image from folder
     // fs.unlink(`/Users/John/Desktop/Git/Zoe-Redux-Backend/uploads/${file_name}`, (err) => {
-    fs.unlink(`../Zoe-Redux-Backend/uploads/${file_name}`, (err) => {
+    fs.unlink(`../Zoe-B/uploads/${file_name}`, (err) => {
         if (err) throw err;
       });
       
@@ -163,7 +163,7 @@ upload.single("uimage"),
 
     //deleting previous image from folder
     // fs.unlink(`/Users/John/Desktop/Git/Zoe-Redux-Backend/uploads/${previousFile}`, (err) => {
-    fs.unlink(`../Zoe-Redux-Backend/uploads/${previousFile}`, (err) => { 
+    fs.unlink(`../Zoe-B/uploads/${previousFile}`, (err) => { 
       if (err) throw err;
     }); 
   
