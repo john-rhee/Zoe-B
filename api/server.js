9e66acff021ef4d54d56f8a87416ce9516ae8fe6
Johnnyr81 <emailjohn81@gmail.com>
2020-08-28 01:51:53 -0500

    change acao to *

--- api/server.js
+++ api/server.js
@@ -15,7 +15,7 @@ server.use(express.json());
 // server.options('*', cors());
 
 server.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", 'https://zoe-kappa.now.sh');
+    res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Headers", '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     // if (req.method==="OPTIONS"){
